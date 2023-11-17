76ee6c5d06c59162baf9d849da5257c3b247286c
Deycov <53343872+adrianA-R@users.noreply.github.com>
2023-11-17 00:21:47 -0400

    Deploy

--- src/app.js
+++ src/app.js
@@ -2,24 +2,23 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 const routes = require("./routes");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname,'public')));
-app.set('views', path.join(__dirname + '/views'));
-app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "ejs");
 
 routes.routesApp(app);
 
-app.get("/",(req,res)=>{
-  res.redirect('v1/calendar');
-})
+app.get("/", (req, res) => {
+  res.redirect("v1/calendar");
+});
 
-
-app.listen(port,()=>{
-  console.log("Programa ejecutandose en el puerto ",port);
+app.listen(port, () => {
+  console.log("Programa ejecutandose en el puerto ", port);
 });
